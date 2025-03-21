336f96a01e0f9f2d451cca02fccaca2ff9e3e797
ScienceKyle <johnson.2937@gmail.com>
2025-03-20 20:55:15 -0400

    Reinitialize repository after accidental deletion

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Homelab Setup",
+        "excerpt":"I’m building a homelab for virtualization and development of Ai applications and other random widgets. Cisco 24port POE gigabit switch pfSense Intel(R) Core(TM) i5-7500 CPU 3.40GHZ Intel i350-T4 4x gigabit NIC Supermicro X11DPH-T Dual Xeon Gold 6138 (40C/80T, 2.0GHz, 3.7GHz boost) 160GB Hyinx DDR4-2666 ECC (10x16gb) 4x gtx1080 Dual 10GbE...","categories": ["homelab"],
+        "tags": [],
+        "url": "/homelab/post-1/",
+        "teaser": "/assets/images/Logo.png"
+      }]
