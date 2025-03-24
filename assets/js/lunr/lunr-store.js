7b8d8933b2bb2ea07f43ed8fe875398560e470f0
ScienceKyle <johnson.2937@gmail.com>
2025-03-24 09:43:15 -0400

    Automated Jekyll build and deploy

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -3,17 +3,23 @@ var store = [{
         "excerpt":"I’m building a homelab for virtualization and development of Ai applications and other random widgets. Cisco 24port POE gigabit switch pfSense Intel(R) Core(TM) i5-7500 CPU 3.40GHZ Intel i350-T4 4x gigabit NIC Supermicro X11DPH-T Dual Xeon Gold 6138 (40C/80T, 2.0GHz, 3.7GHz boost) 160GB Hyinx DDR4-2666 ECC (10x16gb) 4x gtx1080 Dual 10GbE...","categories": ["homelab"],
         "tags": [],
         "url": "/homelab/post-1/",
-        "teaser": "/assets/images/Logo.png"
+        "teaser": "/assets/images/Logo.svg"
       },{
         "title": "Favicon Script",
-        "excerpt":"Hopefully your browser tab shows my makerlabs logo. I used this script to generate a multilayer favicon and put it in the site root ditectory. It uses the imagemagick pkg on termux shell Copy Code cd assets/images/ magick Logo.png -resize 16x16 favicon-16.png magick Logo.png -resize 32x32 favicon-32.png magick Logo.png -resize...","categories": [],
+        "excerpt":"&nbsp;&nbsp;&nbsp;&nbsp;Hopefully your browser tab shows my makerlabs logo. I used a shell script to generate a multilayer favicon and told your browser to use it. A favicon, short for \"favorite icon,\" is a small image associated with a website. It appears in browser tabs, bookmarks, and other locations to identify...","categories": [],
         "tags": [],
         "url": "/favicon-script/",
-        "teaser": "/assets/images/Logo.png"
+        "teaser": "/assets/images/Logo.svg"
       },{
         "title": "Custom Code Container",
         "excerpt":"I wasn't happy with how code was formatting in these posts. ChatGPT was able to help me extend Rouge highlighting built into Jekyll. I created a custom _include/after-content.html instead of modifying the default layout. The change gets applied to all &#123;% highlight xxxx %&#125; sections and gets implemented at build...","categories": ["post"],
         "tags": [],
         "url": "/post/code-preview/",
-        "teaser": "/assets/images/Logo.png"
+        "teaser": "/assets/images/Logo.svg"
+      },{
+        "title": "password",
+        "excerpt":"Secure Password Generator   Use this tool to generate a secure password with customizable options.   Password Length:         Include Uppercase Letters (A-Z)        Include Lowercase Letters (a-z)        Include Numbers (0-9)        Include Symbols (!@#$%^&amp;*)        Generate Password   Generated Password:       ","categories": ["post"],
+        "tags": [],
+        "url": "/post/password/",
+        "teaser": "/assets/images/Logo.svg"
       }]
