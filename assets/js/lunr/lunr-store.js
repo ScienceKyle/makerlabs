a806acd8b6c02234aef1471ee131d3118a7d937f
ScienceKyle <johnson.2937@gmail.com>
2025-03-25 01:11:56 -0400

    Automated Jekyll build and deploy

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,31 +1,61 @@
 var store = [{
+        "title": "Contact",
+        "excerpt":"     Makerlabsllc@gmail.com   ","categories": [],
+        "tags": [],
+        "url": "/contact/",
+        "teaser": "/assets/images/Logo.svg"
+      },{
+        "title": "Default",
+        "excerpt":"     Oops...     Page not found :(    I must be working on this.   ","categories": [],
+        "tags": [],
+        "url": "/default/",
+        "teaser": "/assets/images/Logo.svg"
+      },{
+        "title": "Gallery",
+        "excerpt":"Photo Gallery   Loading...               ","categories": [],
+        "tags": [],
+        "url": "/gallery/",
+        "teaser": "/assets/images/Logo.svg"
+      },{
+        "title": "Tools",
+        "excerpt":"          Password Generator - Generate random secure password            Thesaurus - Generate secure passwords with customizable settings.            Unit Converter - Mostly useful unit converter.      ","categories": [],
+        "tags": [],
+        "url": "/tools/",
+        "teaser": "/assets/images/Logo.svg"
+      },{
+        "title": "Password Generator",
+        "excerpt":"           Password Length:            Generate Password         ","categories": [],
+        "tags": [],
+        "url": "/tools/password_gen/",
+        "teaser": "/assets/images/Logo.svg"
+      },{
+        "title": "Thesaurus",
+        "excerpt":"Advanced Thesaurus Lookup   Select a query type, enter a word, and see results!        Synonyms     Similar Meaning     Sound Like     Related Adjectives     Related Nouns      Words that follow     Associated Words     Spelling Suggestions     Search   Results:      ","categories": [],
+        "tags": [],
+        "url": "/tools/thesaurus/",
+        "teaser": "/assets/images/Logo.svg"
+      },{
+        "title": "Unit Converter",
+        "excerpt":"      ","categories": [],
+        "tags": [],
+        "url": "/tools/units/",
+        "teaser": "/assets/images/Logo.svg"
+      },{
         "title": "Homelab Setup",
         "excerpt":"I’m building a homelab for virtualization and development of Ai applications and other random widgets. Cisco 24port POE gigabit switch pfSense Intel(R) Core(TM) i5-7500 CPU 3.40GHZ Intel i350-T4 4x gigabit NIC Supermicro X11DPH-T Dual Xeon Gold 6138 (40C/80T, 2.0GHz, 3.7GHz boost) 160GB Hyinx DDR4-2666 ECC (10x16gb) 4x gtx1080 Dual 10GbE...","categories": ["homelab"],
         "tags": [],
-        "url": "/homelab/post-1/",
+        "url": "/homelab/2025/03/17/post-1.html",
         "teaser": "/assets/images/Logo.svg"
       },{
         "title": "Favicon Script",
         "excerpt":"&nbsp;&nbsp;&nbsp;&nbsp;Hopefully your browser tab shows my makerlabs logo. I used a shell script to generate a multilayer favicon and told your browser to use it. A favicon, short for \"favorite icon,\" is a small image associated with a website. It appears in browser tabs, bookmarks, and other locations to identify...","categories": [],
         "tags": [],
-        "url": "/favicon-script/",
+        "url": "/2025/03/21/favicon-script.html",
         "teaser": "/assets/images/Logo.svg"
       },{
         "title": "Custom Code Container",
         "excerpt":"I wasn't happy with how code was formatting in these posts. ChatGPT was able to help me extend Rouge highlighting built into Jekyll. I created a custom _include/after-content.html instead of modifying the default layout. The change gets applied to all &#123;% highlight xxxx %&#125; sections and gets implemented at build...","categories": ["post"],
         "tags": [],
-        "url": "/post/code-preview/",
+        "url": "/post/2025/03/21/code-preview.html",
         "teaser": "/assets/images/Logo.svg"
-      },{
-        "title": "Password Generator",
-        "excerpt":"Use this tool to generate a secure password with customizable options.   Password Length:      Generate Password        ","categories": ["post"],
-        "tags": [],
-        "url": "/post/Password-Generator/",
-        "teaser": "/assets/images/Logo.svg"
-      },{
-        "title": "Thesaurus",
-        "excerpt":"Advanced Thesaurus Lookup   Select a query type, enter a word, and see results!        Synonyms     Similar Meaning     Sound Like     Related Adjectives     Related Nouns      Words that follow     Associated Words     Spelling Suggestions     Search   Results:      ","categories": ["post"],
-        "tags": [],
-        "url": "/post/thesaurus/",
-        "teaser": "/assets/images/Logo.svg"
-      }]
+      },]
